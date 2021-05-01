f4700d886aea0b06b3bd282dcc8302167335133c
shogo6768 <73920107+shogo6768@users.noreply.github.com>
2021-05-02 00:04:24 +0900

    Update views.py

--- snsapp/views.py
+++ snsapp/views.py
@@ -133,7 +133,7 @@ class FollowList(LoginRequiredMixin, ListView):
         return Post.objects.filter(user__in=all_follow)
         
         """
-        元々のコード(all_followより以下)
+        元々のコード(all_follow = ...より以下)
         for user in all_follow:
            return Post.objects.filter(user=user)
         
